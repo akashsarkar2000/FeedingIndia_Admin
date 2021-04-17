bc552b605c677b2e77cded0e59ed7416fd17cd46
thamadaashish1999 <thamadaa.cse@sbjit.edu.in>
2021-04-17 09:34:30 +0530

    app

---
+++ app/src/main/java/com/example/feedingindia_admin/splash_screen_admin.java
@@ -0,0 +1,26 @@
+package com.example.feedingindia_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen_admin extends AppCompatActivity {
+    private static int splash = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_admin);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen_admin.this,LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        },splash);
+
+    }
+}
