d951f5665f66baea0df75b62e674128e497f3be6
thamadaashish1999 <thamadaa.cse@sbjit.edu.in>
2021-04-17 09:36:33 +0530

    app

--- app/src/main/java/com/example/feedingindia_admin/splash_screen_admin.java
+++ app/src/main/java/com/example/feedingindia_admin/splash_screen_admin.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class splash_screen_admin extends AppCompatActivity {
-    private static int splash = 3000;
+    private static int splash = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
